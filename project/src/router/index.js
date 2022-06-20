e856aa8568cdb39e5594f32abb7f18d5c5c0437c
김종빈 <been.kim@twolinecode.com>
2022-06-20 14:16:23 +0900

    [ADD] 라우터

--- project/src/router/index.js
+++ project/src/router/index.js
@@ -14,6 +14,16 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/data_binding/string',
+    name: 'data_binding_string_view',
+    component: () => import(/* webpackChunkName: "data_binding", webpackPrefetch:true */ '../views/1_databinding/DataBindingStringView.vue')
+  },
+  {
+    path: '/data_binding/html',
+    name: 'data_binding_html_view',
+    component: () => import(/* webpackChunkName: "data_binding", webpackPrefetch:true */ '../views/1_databinding/DataBindingHtmlView.vue')
   }
 ]
 
---
+++ project/src/views/1_databinding/DataBindingHtmlView.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>{{htmlString}}</div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      htmlString: '<p style="color:red;"> 빨간색 문자 </p>'
+    }
+  }
+
+}
+</script>
